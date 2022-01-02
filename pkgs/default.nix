00e3489cc7ea1edc3e71aaa94a62efbd13d65e49
Karel Kočí <cynerd@email.cz>
2022-01-02 17:22:58 +0100

    pkgs/logc-0_1: use override to instead of attributes

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -13,10 +13,14 @@ let
     ) { };
 
     logc = callPackage ./libraries/logc { };
-    logc-0_1 = callPackage ./libraries/logc {
-      pkgversion = "0.1.0";
-      pkgsha256 = "1swjzs2249wvnqx2zvxwd7d1z22kd3512xxfvq002cvgbq78ka9a";
-    };
+    logc-0_1 = logc.overrideAttrs (oldAttrs: rec {
+      version = "0.1.0";
+      src = fetchgit {
+        url = "https://gitlab.nic.cz/turris/logc.git";
+        rev = "v" + version;
+        sha256 = "1swjzs2249wvnqx2zvxwd7d1z22kd3512xxfvq002cvgbq78ka9a";
+      };
+    });
     logc-libs = callPackage ./libraries/logc-libs { };
     base64c = callPackage ./libraries/base64c { };
     paho-mqtt-c = callPackage ./libraries/paho-mqtt-c { };
--- pkgs/libraries/logc/default.nix
+++ pkgs/libraries/logc/default.nix
@@ -1,12 +1,11 @@
 { stdenv, lib, fetchgit
 , bootstrapHook, pkg-config, gperf
 , libconfig
-, pkgversion ? "0.4.0", pkgsha256 ? "15nplgjgg6dxryy4yzbj4524y77ci0syi970rmbr955m9vxvhrib"
 }:
 
 stdenv.mkDerivation rec {
   pname = "logc";
-  version = pkgversion;
+  version = "0.4.0";
   meta = with lib; {
     homepage = "https://gitlab.nic.cz/turris/logc";
     description = "Logging for C";
@@ -17,7 +16,7 @@ stdenv.mkDerivation rec {
   src = fetchgit {
     url = "https://gitlab.nic.cz/turris/logc.git";
     rev = "v" + version;
-    sha256 = pkgsha256;
+    sha256 = "15nplgjgg6dxryy4yzbj4524y77ci0syi970rmbr955m9vxvhrib";
   };
 
   buildInputs = [libconfig];
