b2099a33684c998d7b3375677f338ba8d628b430
Karel Kočí <cynerd@email.cz>
2022-05-06 13:20:10 +0200

    flake: drop the stable nixos

--- flake.nix
+++ flake.nix
@@ -1,9 +1,7 @@
 {
   description = "Turris flake";
 
-  inputs.nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-21.11";
-
-  outputs = { self, flake-utils, nixpkgs, nixpkgs-stable }: {
+  outputs = { self, flake-utils, nixpkgs }: {
 
       overlays.default = final: prev: import ./pkgs { nixpkgs = prev; };
       overlay = self.overlays.default; # Backward compatibility
@@ -19,8 +17,6 @@
       nixosConfigurations = {
         tarballMox = self.lib.nixturrisTarballSystem { board = "mox"; nixpkgs = nixpkgs; };
         tarballOmnia = self.lib.nixturrisTarballSystem { board = "omnia"; nixpkgs = nixpkgs; };
-        stableTarballMox = self.lib.nixturrisTarballSystem { board = "mox"; nixpkgs = nixpkgs-stable; };
-        stableTarballOmnia = self.lib.nixturrisTarballSystem { board = "omnia"; nixpkgs = nixpkgs-stable; };
       };
 
     } // flake-utils.lib.eachSystem (flake-utils.lib.defaultSystems ++ ["armv7l-linux"]) (
@@ -31,13 +27,9 @@
 
           tarballMox = tarball self.nixosConfigurations.tarballMox;
           tarballOmnia = tarball self.nixosConfigurations.tarballOmnia;
-          stableTarballMox = tarball self.nixosConfigurations.stableTarballMox;
-          stableTarballOmnia = tarball self.nixosConfigurations.stableTarballOmnia;
 
           crossTarballMox = tarball (self.lib.nixturrisTarballSystem { board = "mox"; nixpkgs = nixpkgs; system = system; });
           crossTarballOmnia = tarball (self.lib.nixturrisTarballSystem { board = "omnia"; nixpkgs = nixpkgs; system = system; });
-          stableCrossTarballMox = tarball (self.lib.nixturrisTarballSystem { board = "mox"; nixpkgs = nixpkgs-stable; system = system; });
-          stableCrossTarballOmnia = tarball (self.lib.nixturrisTarballSystem { board = "omnia"; nixpkgs = nixpkgs-stable; system = system; });
 
         } // flake-utils.lib.filterPackages system (flake-utils.lib.flattenTree (
           import ./pkgs { nixpkgs = nixpkgs.legacyPackages."${system}"; }
