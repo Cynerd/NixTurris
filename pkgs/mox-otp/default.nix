f0a0bbb06d7e5c345ad8887dbd5f713cdf467ce3
Karel Kočí <cynerd@email.cz>
2022-10-03 21:49:16 +0200

    mox-otp: update to the latert commit

--- pkgs/mox-otp/default.nix
+++ pkgs/mox-otp/default.nix
@@ -4,7 +4,7 @@
 
 buildPythonApplication rec {
   pname = "mox-otp";
-  version = "0.3";
+  version = "0.3.1-pre0";
   meta = with lib; {
     homepage = "https://gitlab.nic.cz/turris/mox-otp";
     description = "Command line tool to query MOX CPU read-only OTP device";
@@ -14,7 +14,8 @@ buildPythonApplication rec {
 
   src = fetchgit {
     url = "https://gitlab.nic.cz/turris/mox-otp.git";
-    rev = "v" + version;
-    sha256 = "12k9mgv1kmv9cawgx0ccq4m4liqizszvvsl457jkkirb7rbzhw5y";
+    rev = "ac3c4ae89d3d7e7ca2c4cc1ad97cdbc9936a1026";
+    #rev = "v" + version;
+    sha256 = "0sknaqv7aga99x99mh5gvinx3fc4rl9pyaq8j71nmhblf50cfwk4";
   };
 }
