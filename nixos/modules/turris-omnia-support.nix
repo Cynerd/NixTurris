fc435c8d4ed77f2949d944f037a876b7043bcac5
Karel Kočí <cynerd@email.cz>
2022-10-23 16:00:36 +0200

    nixos/turris-omnia-support: load turris omnia leds module

--- nixos/modules/turris-omnia-support.nix
+++ nixos/modules/turris-omnia-support.nix
@@ -9,6 +9,11 @@ with lib;
     boot.kernelParams = [
       "earlyprintk" "console=ttyS0,115200"
     ];
+    # Force load of Turris Omnia leds (not loadded automatically for some
+    # reason).
+    boot.kernelModules = [
+      "leds_turris_omnia"
+    ];
 
     # The additional administration packages
     environment.systemPackages =  with pkgs; [
