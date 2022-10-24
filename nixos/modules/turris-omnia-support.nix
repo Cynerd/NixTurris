b95e8b11cfff1397b784781c83a3d736cde4a9ac
Karel Kočí <cynerd@email.cz>
2022-10-24 21:45:38 +0200

    nixos/turris-omnia-support: omnia should be fixed now

--- nixos/modules/turris-omnia-support.nix
+++ nixos/modules/turris-omnia-support.nix
@@ -9,13 +9,10 @@ with lib;
     boot.kernelParams = [
       "earlyprintk" "console=ttyS0,115200"
     ];
-    # Force load of Turris Omnia leds (not loadded automatically for some
-    # reason).
-    #boot.kernelModules = [
-    #  "leds_turris_omnia"
-    #];
-    # Blacklist leds for now as they cause kernel faults
-    boot.blacklistedKernelModules = ["leds_turris_omnia"];
+    # Force load of Turris Omnia leds
+    boot.kernelModules = [
+      "leds_turris_omnia"
+    ];
     # Explicitly set device tree to ensure we load the correct one.
     hardware.deviceTree.name = "armada-385-turris-omnia.dtb";
 
