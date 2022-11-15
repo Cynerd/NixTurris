59c6e20c4f7004a54d03a6e48d47023f338e08f1
Karel Kočí <cynerd@email.cz>
2022-11-15 22:50:08 +0100

    nixos/turris-kernel-patches: add completion timeout workaround

--- nixos/modules/turris-kernel-patches.nix
+++ nixos/modules/turris-kernel-patches.nix
@@ -7,6 +7,10 @@
 with lib; {
   boot.kernelPatches = [
     {
+      name = "PCI: aardvark: Implement workaround for PCIe Completion Timeout";
+      patch = ./v2-PCI-aardvark-Implement-workaround-for-PCIe-Completion-Timeout.patch;
+    }
+    {
       name = "PCI-aadvark";
       patch = ./PCI-aardvark-controller-changes-BATCH-6.patch;
     }
