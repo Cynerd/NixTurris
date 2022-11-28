3adb11a1e656d369943d1688aa042d900c3ac6cc
Karel Kočí <cynerd@email.cz>
2022-11-28 14:26:32 +0100

    nixos: tweak LEDs modules name to fit with support module

--- nixos/default.nix
+++ nixos/default.nix
@@ -4,9 +4,9 @@ self: let
     turris-crossbuild = import ./modules/turris-crossbuild.nix;
     turris-defaults = import ./modules/turris-defaults.nix;
     turris-mox-support = import ./modules/turris-mox-support.nix;
-    turris-moxled = import ./modules/turris-moxled.nix;
+    turris-mox-led = import ./modules/turris-mox-led.nix;
     turris-omnia-support = import ./modules/turris-omnia-support.nix;
-    turris-omnialeds = import ./modules/turris-omnialeds.nix;
+    turris-omnia-leds = import ./modules/turris-omnia-leds.nix;
     turris-tarball = import ./modules/turris-tarball.nix;
 
     armv7l-overlay = import ./modules/armv7l-overlay.nix;
