4f39d087b5d94fd30e7797a730dac258f636890b
Karel Kočí <cynerd@email.cz>
2022-12-05 11:04:11 +0100

    nixos/armv7l-overlay: try the native build again
    The native build works now with this but for some reason tarball can't
    be build.

--- nixos/modules/armv7l-overlay.nix
+++ nixos/modules/armv7l-overlay.nix
@@ -17,13 +17,16 @@ in {
             doInstallCheck = false;
           });
       in {
+        # Overrides to get build to work
         boehmgc = disableCheck super.boehmgc;
-        libseccomp = disableCheck super.libseccomp;
         libuv = disableCheck super.libuv;
+        dav1d = disableCheck super.dav1d;
         elfutils = disableCheck super.elfutils;
-        gobject-introspection = disableCheck super.gobject-introspection;
         nlohmann_json = disableCheck super.nlohmann_json;
-        openldap = disableCheck super.openldap;
+        gobject-introspection = disableCheck super.gobject-introspection;
+        mdbook = disableCheck super.mdbook;
+        libseccomp = disableCheck super.libseccomp;
+        nix = disableCheck super.nix;
 
         python310 = super.python310.override {
           packageOverrides = python-self: python-super: let
@@ -40,28 +43,6 @@ in {
         python310Packages = self.python310.pkgs;
         python = self.python310;
         pythonPackages = self.python.pkgs;
-
-        # Overrides to get build to work
-        #boehmgc = armv7lDisableCheck nixpkgs.boehmgc;
-        #libseccomp = armv7lDisableCheck nixpkgs.libseccomp;
-        #libuv = armv7lDisableCheck nixpkgs.libuv;
-        #elfutils = armv7lDisableCheck nixpkgs.elfutils;
-        #patchelf = armv7lDisableCheck nixpkgs.patchelf;
-        #bison = armv7lDisableCheck nixpkgs.bison;
-        #findutils = armv7lDisableCheck nixpkgs.findutils;
-        #p11-kit = armv7lDisableCheck nixpkgs.p11-kit;
-        #glib = armv7lDisableCheck nixpkgs.glib;
-        #rustc = armv7lDisableCheck nixpkgs.rustc;
-        #mdbook = armv7lDisableCheck nixpkgs.mdbook;
-        #ell = armv7lDisableCheck nixpkgs.ell;
-        #polkit = armv7lDisableCheck nixpkgs.polkit;
-        #udisks2 = disableCheck nixpkgs.udisks2;
-        #udisks = udisks2;
-        #llvm = armv7lDisableCheck nixpkgs.llvm;
-        #llvm_14 = armv7lDisableCheck nixpkgs.llvm_14;
-        #jemalloc = armv7lDisableCheck nixpkgs.jemalloc;
-        #openssh = armv7lDisableCheck nixpkgs.openssh;
-        #nlohmann_json = armv7lDisableCheck nixpkgs.nlohmann_json;
       })
     ])
     ++ (optionals is_cross [
