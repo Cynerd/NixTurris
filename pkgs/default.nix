db673b58826082bfa04e8f8b4281d5172cc2a680
Karel Kočí <cynerd@email.cz>
2023-01-16 21:04:01 +0100

    pkgs: drop Linux 6.0 as it is EOL

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -30,22 +30,12 @@ with nixpkgs.lib; let
         patch = ./patches/linux-omnia-separate-dts.patch;
       };
     };
-    linux_6_0_turris_omnia = nixpkgs.linux_6_0.override (oldAttrs: {
-      kernelPatches = [
-        turrispkgs.kernelPatchesTurris.mvebu_pci_omnia_fix
-        turrispkgs.kernelPatchesTurris.omnia_separate_dtb
-      ];
-      features.turrisOmniaSplitDTB = true;
-    });
     linux_6_1_turris_omnia = nixpkgs.linux_6_1.override (oldAttrs: {
       kernelPatches = [
         turrispkgs.kernelPatchesTurris.mvebu_pci_omnia_fix
         turrispkgs.kernelPatchesTurris.omnia_separate_dtb
       ];
       features.turrisOmniaSplitDTB = true;
-    });
-    linux_6_0_turris_mox = nixpkgs.linux_6_0.override (oldAttrs: {
-      kernelPatches = [turrispkgs.kernelPatchesTurris.mvebu_pci_aadvark];
     });
     linux_6_1_turris_mox = nixpkgs.linux_6_1.override (oldAttrs: {
       kernelPatches = [turrispkgs.kernelPatchesTurris.mvebu_pci_aadvark];
