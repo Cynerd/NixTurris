3459b5cbef1b770fad834fdd7d4182e834e643dc
Karel Kočí <cynerd@email.cz>
2024-03-06 17:17:43 +0100

    nixos: do not import modules
    This has advantage that NixOS correctly reports source of the settings
    instead of just saying that it is from top level flake.

--- nixos/default.nix
+++ nixos/default.nix
@@ -1,15 +1,15 @@
 self: let
   modules = {
-    turris-board = import ./modules/turris-board.nix;
-    turris-defaults = import ./modules/turris-defaults.nix;
-    turris-install = import ./modules/turris-install.nix;
-    turris-mox-led = import ./modules/turris-mox-led.nix;
-    turris-mox-support = import ./modules/turris-mox-support.nix;
-    turris-omnia-leds = import ./modules/turris-omnia-leds.nix;
-    turris-omnia-support = import ./modules/turris-omnia-support.nix;
-    turris-tarball = import ./modules/turris-tarball.nix;
+    turris-board = ./modules/turris-board.nix;
+    turris-defaults = ./modules/turris-defaults.nix;
+    turris-install = ./modules/turris-install.nix;
+    turris-mox-led = ./modules/turris-mox-led.nix;
+    turris-mox-support = ./modules/turris-mox-support.nix;
+    turris-omnia-leds = ./modules/turris-omnia-leds.nix;
+    turris-omnia-support = ./modules/turris-omnia-support.nix;
+    turris-tarball = ./modules/turris-tarball.nix;
 
-    fwenv = import ./modules/fwenv.nix;
+    fwenv = ./modules/fwenv.nix;
   };
 in
   modules
