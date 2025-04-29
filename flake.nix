7da0b4be8e2b7a0f5da5d6ea7d39ffdbff54ca93
Karel Kočí <cynerd@email.cz>
2025-04-29 16:29:31 +0200

    flake: remove flake-utils
    We are using it only to iterate over systems. We can use flakeExposed
    from nixpkgs to do the same so remove the dependency on flake-inputs.

--- flake.nix
+++ flake.nix
@@ -3,51 +3,44 @@
 
   outputs = {
     self,
-    flake-utils,
     nixpkgs,
   }: let
-    inherit (flake-utils.lib) eachDefaultSystem;
-    inherit (nixpkgs.lib) fixedPoints;
-  in
-    {
-      overlays = {
-        lib = final: prev: import ./lib self final prev;
-        default = final: prev: import ./pkgs prev final;
+    inherit (nixpkgs.lib) genAttrs fixedPoints systems;
+    forSystems = genAttrs systems.flakeExposed;
+  in {
+    overlays = {
+      lib = final: prev: import ./lib self final prev;
+      default = final: prev: import ./pkgs prev final;
+    };
+    nixosModules = import ./nixos self;
+    lib = fixedPoints.fix (import ./lib self nixpkgs.lib);
+
+    nixosConfigurations = {
+      installMox = self.lib.nixturrisSystem {
+        board = "mox";
+        modules = [{turris.install-settings = true;}];
       };
-      nixosModules = import ./nixos self;
-      lib = fixedPoints.fix (import ./lib self nixpkgs.lib);
-
-      nixosConfigurations = {
-        installMox = self.lib.nixturrisSystem {
-          board = "mox";
-          modules = [{turris.install-settings = true;}];
-        };
-        installOmnia = self.lib.nixturrisSystem {
-          board = "omnia";
-          modules = [{turris.install-settings = true;}];
-        };
+      installOmnia = self.lib.nixturrisSystem {
+        board = "omnia";
+        modules = [{turris.install-settings = true;}];
       };
+    };
 
-      templates.default = {
-        path = ./template;
-        description = "NixOS configuration for Turris";
-      };
-    }
-    // eachDefaultSystem (
-      system: let
-        pkgs = nixpkgs.legacyPackages."${system}";
-        tarball = nixos: nixos.buildPlatform.${system}.config.system.build.tarball;
-      in {
-        packages = {
-          tarballMox = tarball self.nixosConfigurations.installMox;
-          tarballOmnia = tarball self.nixosConfigurations.installOmnia;
-        };
+    templates.default = {
+      path = ./template;
+      description = "NixOS configuration for Turris";
+    };
 
-        # The legacyPackages imported as overlay allows us to use pkgsCross to
-        # cross-compile those packages.
-        legacyPackages = pkgs.extend self.overlays.default;
+    packages = forSystems (system: let
+      tarball = nixos: nixos.buildPlatform.${system}.config.system.build.tarball;
+    in {
+      tarballMox = tarball self.nixosConfigurations.installMox;
+      tarballOmnia = tarball self.nixosConfigurations.installOmnia;
+    });
+    legacyPackages =
+      forSystems (system:
+        nixpkgs.legacyPackages.${system}.extend self.overlays.default);
 
-        formatter = pkgs.alejandra;
-      }
-    );
+    formatter = forSystems (system: nixpkgs.legacyPackages.${system}.alejandra);
+  };
 }
