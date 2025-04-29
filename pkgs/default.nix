f74cb7f5bdd0a6d29662c78098be1b44abd3a0e0
Karel Kočí <cynerd@email.cz>
2025-04-29 18:18:33 +0200

    pkgs: use overlay as expected over self referencing
    This ensures that you can overlay armTrustedFirmwareTurrisMox.
    
    This also adds linux_6_12_* for the latest LTS version. They are build
    tested but not run tested yet.

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -37,7 +37,9 @@ prev: final: let
   };
   overrideMox = kernel:
     kernel.override (oldAttrs: {
-      kernelPatches = oldAttrs.kernelPatches ++ (optional (versionOlder kernel.version "6.2") kernelPatchesTurris.mvebu_pci_aadvark);
+      kernelPatches =
+        oldAttrs.kernelPatches
+        ++ (optional (versionOlder kernel.version "6.2") kernelPatchesTurris.mvebu_pci_aadvark);
     });
   overrideOmnia = kernel:
     kernel.override (oldAttrs: {
@@ -54,63 +56,62 @@ prev: final: let
         ];
       features.turrisOmniaSplitDTB = true;
     });
+in {
+  # Crypto and certificates
+  libatsha204 = final.callPackage ./libatsha204 {};
+  mox-otp = final.python3Packages.callPackage ./mox-otp {};
+  crypto-wrapper = final.callPackage ./crypto-wrapper {};
 
-  turrispkgs = {
-    # Crypto and certificates
-    libatsha204 = final.callPackage ./libatsha204 {};
-    mox-otp = final.python3Packages.callPackage ./mox-otp {};
-    crypto-wrapper = final.callPackage ./crypto-wrapper {};
+  # Kernel patches and board specific kernels
+  inherit kernelPatchesTurris;
+  # Mox kernels
+  linux_turris_mox = overrideMox prev.linux;
+  linux_latest_turris_mox = overrideMox prev.linux_latest;
+  linux_6_1_turris_mox = overrideMox prev.linux_6_1;
+  linux_6_6_turris_mox = overrideMox prev.linux_6_6;
+  linux_6_12_turris_mox = overrideMox prev.linux_6_12;
+  # Omnia kernels
+  linux_turris_omnia = overrideOmnia prev.linux;
+  linux_latest_turris_omnia = overrideOmnia prev.linux_latest;
+  linux_6_1_turris_omnia = overrideOmnia prev.linux_6_1;
+  linux_6_6_turris_omnia = overrideOmnia prev.linux_6_6;
+  linux_6_12_turris_omnia = overrideOmnia prev.linux_6_12;
 
-    # Kernel patches and board specific kernels
-    inherit kernelPatchesTurris;
-    # Mox kernels
-    linux_turris_mox = overrideMox prev.linux;
-    linux_latest_turris_mox = overrideMox prev.linux_latest;
-    linux_6_1_turris_mox = overrideMox prev.linux_6_1;
-    linux_6_6_turris_mox = overrideMox prev.linux_6_6;
-    # Omnia kernels
-    linux_turris_omnia = overrideOmnia prev.linux;
-    linux_latest_turris_omnia = overrideOmnia prev.linux_latest;
-    linux_6_1_turris_omnia = overrideOmnia prev.linux_6_1;
-    linux_6_6_turris_omnia = overrideOmnia prev.linux_6_6;
+  # NOR Firmware as considered stable by Turris and shipped in Turris OS
+  tosFirmwareOmnia = final.callPackage ./tos-firmware {board = "omnia";};
+  tosFirmwareMox = final.callPackage ./tos-firmware {board = "mox";};
 
-    # NOR Firmware as considered stable by Turris and shipped in Turris OS
-    tosFirmwareOmnia = final.callPackage ./tos-firmware {board = "omnia";};
-    tosFirmwareMox = final.callPackage ./tos-firmware {board = "mox";};
-
-    # NOR Firmwares build in Nix
-    armTrustedFirmwareTurrisMox = prev.buildArmTrustedFirmware rec {
-      platform = "a3700";
-      extraMeta.platforms = ["aarch64-linux"];
-      extraMakeFlags = ["USE_COHERENT_MEM=0" "CM3_SYSTEM_RESET=1" "FIP_ALIGN=0x100"];
-      filesToInstall = ["build/${platform}/release/bl31.bin"];
-    };
-    ubootTurrisMox = prev.buildUBoot {
-      defconfig = "turris_mox_defconfig";
-      extraMeta.platforms = ["aarch64-linux"];
-      filesToInstall = ["u-boot.bin"];
-      extraPatches = [./patches/include-configs-turris_mox-increase-space-for-the-ke.patch];
-      BL31 = "${turrispkgs.armTrustedFirmwareTurrisMox}/bl31.bin";
-    };
-    ubootTurrisOmnia = prev.buildUBoot {
-      defconfig = "turris_omnia_defconfig";
-      extraMeta.platforms = ["armv7l-linux"];
-      filesToInstall = ["u-boot-spl.kwb"];
-    };
-
-    # Firmware environment tools
-    ubootEnvTools = prev.buildUBoot {
-      defconfig = "tools-only_defconfig";
-      installDir = "$out/bin";
-      hardeningDisable = [];
-      dontStrip = false;
-      extraMeta.platforms = prev.lib.platforms.linux;
-      extraMakeFlags = ["envtools"];
-      filesToInstall = ["tools/env/fw_printenv"];
-      postInstall = ''
-        ln -sf fw_printenv $out/bin/fw_setenv
-      '';
-    };
+  # NOR Firmwares build in Nix
+  armTrustedFirmwareTurrisMox = prev.buildArmTrustedFirmware rec {
+    platform = "a3700";
+    extraMeta.platforms = ["aarch64-linux"];
+    extraMakeFlags = ["USE_COHERENT_MEM=0" "CM3_SYSTEM_RESET=1" "FIP_ALIGN=0x100"];
+    filesToInstall = ["build/${platform}/release/bl31.bin"];
   };
-in
-  turrispkgs
+  ubootTurrisMox = prev.buildUBoot {
+    defconfig = "turris_mox_defconfig";
+    extraMeta.platforms = ["aarch64-linux"];
+    filesToInstall = ["u-boot.bin"];
+    extraPatches = [./patches/include-configs-turris_mox-increase-space-for-the-ke.patch];
+    BL31 = "${final.armTrustedFirmwareTurrisMox}/bl31.bin";
+  };
+  ubootTurrisOmnia = prev.buildUBoot {
+    defconfig = "turris_omnia_defconfig";
+    extraMeta.platforms = ["armv7l-linux"];
+    filesToInstall = ["u-boot-spl.kwb"];
+  };
+
+  # Firmware environment tools
+  ubootEnvTools = prev.buildUBoot {
+    defconfig = "tools-only_defconfig";
+    installDir = "$out/bin";
+    hardeningDisable = [];
+    dontStrip = false;
+    extraMeta.platforms = prev.lib.platforms.linux;
+    extraMakeFlags = ["envtools"];
+    filesToInstall = ["tools/env/fw_printenv"];
+    postInstall = ''
+      ln -sf fw_printenv $out/bin/fw_setenv
+    '';
+  };
+}
