546ac84a3c23d7f0649cece24244985f00c9237f
Karel Kočí <cynerd@email.cz>
2025-04-30 17:42:26 +0200

    nixos/uboot-fwenv: another update
    This fixes version introduced in
    8763be68a2f20d1007344e6c4126a15d4a3d50db. This is the latest upstrema
    version.

--- nixos/modules/uboot-fwenv.nix
+++ nixos/modules/uboot-fwenv.nix
@@ -8,43 +8,43 @@ with lib; let
   cfg = config.programs.uboot-fwenv;
 in {
   options.programs.uboot-fwenv = {
-    enable = mkEnableOption (mdDoc ''
+    enable = mkEnableOption ''
       U-Boot environment access configuration.
 
       Use this to setup access to the U-Boot's environment variables with
       generic ubootEnvTools package.
-    '');
+    '';
     package = mkOption {
       type = types.package;
       default = pkgs.ubootEnvTools;
       defaultText = literalExpression "pkgs.ubootEnvTools";
-      description = mdDoc ''
+      description = ''
         Package providing U-Boot's environment tools (`fw_printenv` and
         `fw_setenv`) that is added to `environment.systemPackages`.
       '';
     };
     device = mkOption {
       type = types.str;
-      description = mdDoc "Device with U-Boot environment";
+      description = "Device with U-Boot environment";
     };
     offset = mkOption {
       type = types.strMatching "0x[a-fA-F0-9]+";
       default = "0x0";
-      description = mdDoc "Offset in the device to the environment";
+      description = "Offset in the device to the environment";
     };
     size = mkOption {
       type = types.strMatching "0x[a-fA-F0-9]+";
-      description = mdDoc "Environemnt size";
+      description = "Environemnt size";
     };
     secsize = mkOption {
       type = with types; nullOr (strMatching "0x[a-fA-F0-9]+");
       default = null;
-      description = mdDoc "Flash sector size.";
+      description = "Flash sector size.";
     };
     numsec = mkOption {
       type = with types; nullOr (strMatching "0x[a-fA-F0-9]+");
       default = null;
-      description = mdDoc "Number of sectors";
+      description = "Number of sectors";
     };
   };
 
