0c48a67c245ab0baf2e8ec36dd6a1c99f1d8e78e
Karel Kočí <cynerd@email.cz>
2025-07-14 13:53:19 +0200

    pkgs/mox-otp: fix build with latest Python packaging changes
    The pyproject is now required.

--- pkgs/mox-otp/default.nix
+++ pkgs/mox-otp/default.nix
@@ -2,20 +2,23 @@
   buildPythonApplication,
   lib,
   fetchgit,
+  setuptools,
 }:
 buildPythonApplication rec {
   pname = "mox-otp";
   version = "0.3.1";
+  src = fetchgit {
+    url = "https://gitlab.nic.cz/turris/mox-otp.git";
+    rev = "v" + version;
+    sha256 = "0sknaqv7aga99x99mh5gvinx3fc4rl9pyaq8j71nmhblf50cfwk4";
+  };
+  pyproject = true;
+  build-system = [setuptools];
+
   meta = with lib; {
     homepage = "https://gitlab.nic.cz/turris/mox-otp";
     description = "Command line tool to query MOX CPU read-only OTP device";
     platforms = platforms.linux;
     license = licenses.gpl3;
-  };
-
-  src = fetchgit {
-    url = "https://gitlab.nic.cz/turris/mox-otp.git";
-    rev = "v" + version;
-    sha256 = "0sknaqv7aga99x99mh5gvinx3fc4rl9pyaq8j71nmhblf50cfwk4";
   };
 }
